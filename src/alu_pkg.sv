167f7daee22c2f09c28e5841b045f52e43abd197
rahulbabel-ssl <rahulbabel14@gmail.com>
2025-07-30 23:00:47 +0530

    Create alu_pkg.sv

---
+++ src/alu_pkg.sv
@@ -0,0 +1,10 @@
+package alu_package;
+        `include "alu_transaction.sv"
+        `include "alu_generator.sv"
+        `include "alu_monitor.sv"
+        `include "alu_driver.sv"
+        `include "alu_scoreboard.sv"
+        `include "alu_reference.sv"
+        `include "alu_environment.sv"
+        `include "alu_test.sv"
+        endpackage
